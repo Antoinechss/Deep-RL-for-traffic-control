563775a4ed4afe90b405e4b213569bf80b62f349
Antoine Chosson <antoine.chosson@eleves.enpc.fr>
2025-04-26 14:47:44 +0200

    Update sumo_env.py

--- Traffic_control/sumo_env.py
+++ Traffic_control/sumo_env.py
@@ -60,20 +60,8 @@ class SumoIntersectionEnv(gym.Env):
         return self._get_obs(), {}
 
     def compute_reward(self):
-        tsd = 0.0
-        for vid in traci.vehicle.getIDList():
-            try:
-                v = traci.vehicle.getSpeed(vid)
-                delay = max(0.0, 1.0 - (v / self.vmax))
-                tsd += delay ** 2
-            except:
-                pass
-
-        # Update tsd_max
-        self.tsd_max = max(self.tsd_max, tsd)
-
-        # Compute normalized reward
-        reward = 1.0 - (tsd / self.tsd_max)
+        kpis = self.get_kpis()
+        reward = -(kpis["waiting_time"] / 20000)
         return reward
 
     def step(self, action):
